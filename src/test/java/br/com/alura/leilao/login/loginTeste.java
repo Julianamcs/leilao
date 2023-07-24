de0bb45e87a149d8b6e1bd072d625e507f940bd9
Juliana Maciel <julyanamcs@gmail.com>
2023-07-24 09:28:44 -0300

    testando a funcionalidade de dados invalidos

--- src/test/java/br/com/alura/leilao/login/loginTeste.java
+++ src/test/java/br/com/alura/leilao/login/loginTeste.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.login;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,6 +22,24 @@ public class loginTeste {
 		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
 		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
 		
+		browser.quit();
+	}
+	
+	
+	@Test
+	public void naoDeverialogarComdadosInvalidos() {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("beltrano");
+		browser.findElement(By.id("password")).sendKeys("1234");
+		//recuperar pela tag form
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		Assert.assertTrue("fulano", browser.getPageSource().contains("Usuário e senha inválidos."));
+		Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
 		browser.quit();
 	}
 
