56465c9b07a2a5d721b07eecbc618aa82fccbf15
Juliana Maciel <julyanamcs@gmail.com>
2023-07-24 09:02:09 -0300

    teste da funcionalidade login

---
+++ src/test/java/br/com/alura/leilao/login/loginTeste.java
@@ -0,0 +1,27 @@
+package br.com.alura.leilao.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginTeste {
+	
+	@Test
+	public void deveriaEfetuarLoginComDadosValidos() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("fulano");
+		browser.findElement(By.id("password")).sendKeys("pass");
+		//recuperar pela tag form
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+		
+		browser.quit();
+	}
+
+}
